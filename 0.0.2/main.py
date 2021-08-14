c8e0703aabf681df291347bd08907553b8bc6cbe
Notsmartname <79399660+Notsmartname@users.noreply.github.com>
2021-08-14 15:26:50 +0500

    'Версия 0.0.2.1 Начало. Вывел первый образец меню'

---
+++ 0.0.2/main.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+
+
+class GlobalMain(BoxLayout):
+    pass
+
+
+class TolicApp(App):
+    pass
+
+
+TolicApp().run()
