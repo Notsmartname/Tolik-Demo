cb321d608fb84d7d9d87fad627ba16b1e6ee449a
Notsmartname <79399660+Notsmartname@users.noreply.github.com>
2021-08-20 13:59:36 +0500

    'Чекпоинт'

--- 0.0.2/main.py
+++ 0.0.2/main.py
@@ -1,10 +1,38 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.widget import Widget
 
 
-class GlobalMain(BoxLayout):
+class WindowManager(ScreenManager):
     pass
+
+
+class GlobalMain(Screen):
+    pass
+
+
+class BetonMain(Screen):
+    pass
+
+
+class KladkaMain(Screen):
+    pass
+
+
+# Расчет отделки помещений
+class OtdelkaMain(Screen):
+    pass
+
+
+class PolMain(Screen):
+    pass
+
+
+class MyLayout(Widget):
+    def spinner_clicked(self, value):
+        self.ids.click_label.text = value
 
 
 class TolicApp(App):
---
+++ testmain/testmain.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+from onemain import OnemainCont
+
+
+class MainMenu(BoxLayout):
+    pass
+
+
+
+class TestApp(App):
+    pass
+
+
+TestApp().run()
